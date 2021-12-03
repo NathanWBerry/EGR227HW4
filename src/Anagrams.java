00eda6640dcc80a1516f492223bde6423ca8ebed
NathanWBerry <nathanwade.berry@calbaptist.edu>
2021-12-03 12:03:18 -0800

    Added the method headings for all of the methods in this assignment.

--- src/Anagrams.java
+++ src/Anagrams.java
@@ -1,6 +1,15 @@
+import java.util.*;
+
 /**
  * Write your comments here
  */
 public class Anagrams {
     //Implement this class
+    public Anagrams(List<String> dictionary) {
+
+    }
+
+    public void print(String text, int max) {
+
+    }
 }
--- src/LetterInventory.java
+++ src/LetterInventory.java
@@ -1,6 +1,51 @@
+import java.util.*;
+import java.lang.reflect.*;
+
 /**
  * Write your comments here
  */
 public class LetterInventory {
     //Implement this class
+    public final int COUNTER = 26;
+
+    public LetterInventory() {
+
+    }
+
+    public LetterInventory(String data) {
+
+    }
+
+    public int get(char letter) {
+        return letter;
+    }
+
+    public void set(char letter, int value) {
+
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public String toString() {
+        return "";
+    }
+
+    public LetterInventory add(LetterInventory other) {
+        return other;
+    }
+
+    public LetterInventory subtract(LetterInventory other) {
+        return other;
+    }
+
+    public double getLetterPercentage(char letter) {
+        return 0.0;
+    }
+
 }
