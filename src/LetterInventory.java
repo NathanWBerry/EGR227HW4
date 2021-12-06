7c2ed147261e305fec37453851e9c006398e7291
NathanWBerry <nathanwade.berry@calbaptist.edu>
2021-12-06 11:38:20 -0800

    Part 1 finished.

--- src/LetterInventory.java
+++ src/LetterInventory.java
@@ -2,7 +2,8 @@ import java.util.*;
 import java.lang.reflect.*;
 
 /**
- * Write your comments here
+ * Nathan Berry
+ * EGR227 HW4
  */
 public class LetterInventory {
     //Implement this class
@@ -18,26 +19,16 @@ public class LetterInventory {
         total = 0;
     }
 
-    private int inventoryHelper(char[] data, char c) {
-        int count = 0;
-        for (int i = 0; i < data.length; i++) {
-            if (data[i] == c) {
-                count++;
-            }
-        }
-        return count;
-    }
-
     public LetterInventory(String data) {
-        /* Setup array */
+        // Initialize the array
         this();
-        /* Iterate over the input String to allocate the appropriate counters */
+        // This loop goes through the string characters to get the total number of counters
         for(int i = 0; i < data.length(); i++) {
             char c = data.charAt(i);
-            /* Consider only letter characters */
+            //This if statement ignores all characters other than letters
             if (Character.isLetter(c)) {
                 c = Character.toLowerCase(c);
-                /* convenient way to get indices from character values */
+                //Calls helper method to easily get index numbers for each character
                 inventory[getIndex(c)]++;
                 total++;
             }
@@ -45,43 +36,70 @@ public class LetterInventory {
     }
 
 
-    public int get(char letter) {
+    public int get(char letter) { //Gets the count for the character entered
         if (!Character.isLetter(letter)) throw new IllegalArgumentException("Must be letters.");
         return inventory[getIndex(letter)];
     }
 
-    private static int getIndex(char letter) {
+    private static int getIndex(char letter) { //Helper method that converts a char into its index in the array
         letter = Character.toLowerCase(letter);
         return (int)letter - (int)'a';
     }
 
-    public void set(char letter, int value) {
+    public void set(char letter, int value) { //This method sets a new value or count to a given char
+        //Throws an error if the char is not a letter or the value is negative
+        if (!Character.isLetter(letter)) throw new IllegalArgumentException("Must be a letter to call this method.");
+        if (value < 0) throw new IllegalArgumentException("Can't input negative numbers.");
 
+        total += value - inventory[getIndex(letter)]; //Sets the new total count
+        inventory[getIndex(letter)] = value; //Sets the new value to the char
     }
 
-    public int size() {
+    public int size() { //this method returns the total amount of counts in the array
         return total;
     }
 
-    public boolean isEmpty() {
+    public boolean isEmpty() { //returns true if the array is empty
         if (total == 0) return true;
         else return false;
     }
 
-    public String toString() {
-        return "";
+    public String toString() { //Converts the array to a string
+        String answer = "[";
+        for (int i = 0; i < COUNTER; i++) { //Loops through all 26 indexes
+            for (int j = 0; j < inventory[i]; j++) {
+                answer += (char)('a' + i); //Converts the index back into characters
+            }
+        }
+        answer += "]";
+        return answer;
     }
 
-    public LetterInventory add(LetterInventory other) {
-        return other;
+    public LetterInventory add(LetterInventory other) { //This method adds the counts of two different LetterInventories together
+        LetterInventory newInventory = new LetterInventory();
+        for (int i = 0; i < COUNTER; i++) {
+            newInventory.inventory[i] = inventory[i] + other.inventory[i]; //Adds the counts
+        }
+        newInventory.total = total + other.total; //Adds the total counts
+        return newInventory;
     }
 
-    public LetterInventory subtract(LetterInventory other) {
-        return other;
+    public LetterInventory subtract(LetterInventory other) { //This method subtracts an inventor
+        LetterInventory newInventory = new LetterInventory();
+        for (int i = 0; i < COUNTER; i++) {
+            if (inventory[i] - other.inventory[i] < 0) {
+                return null;
+            } else {
+                newInventory.inventory[i] = inventory[i] - other.inventory[i];
+            }
+        }
+        newInventory.total = total - other.total;
+        return newInventory;
     }
 
     public double getLetterPercentage(char letter) {
-        return 0.0;
+        double answer = get(letter) / (double)total;
+        return answer;
     }
 
 }
